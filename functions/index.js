590df9fc27af9adddb3e291c4fccebf192b1d533
Tuanhao <tuanhao256@gmail.com>
2021-04-20 15:45:12 +0700

    addition to previous commit

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,11 @@ const {
     addWords
 } = require('./APIs/todos')
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://vn-word-generator.web.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.post('/addWords', addWords)
 app.post('/getWords', getWords)
